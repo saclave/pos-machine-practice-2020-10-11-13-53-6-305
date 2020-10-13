04c046edafb0b43d0dfa2f2c184aa23aa8a24097
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-13 21:50:00 +0800

    [POS v2][Vea] feature: POS machine activtity + Context Map & PDCA (in folder)

--- src/main/java/pos/machine/ItemDataLoader.java
+++ src/main/java/pos/machine/ItemDataLoader.java
@@ -8,11 +8,18 @@ public class ItemDataLoader {
     public static List<ItemInfo> loadAllItemInfos() {
         ItemInfo item1Info = new ItemInfo("ITEM000000", "Coca-Cola", 3);
         ItemInfo item2Info = new ItemInfo("ITEM000001", "Sprite", 3);
-        ItemInfo item3Info = new ItemInfo("ITEM000004", "Battery", 2);
+        ItemInfo item3Info = new ItemInfo("ITEM000002", "Apple", 5);
+        ItemInfo item4Info = new ItemInfo("ITEM000003", "Litchi", 15);
+        ItemInfo item5Info = new ItemInfo("ITEM000004", "Battery", 2);
+        ItemInfo item6Info = new ItemInfo("ITEM000005", "Instant Noodles", 4);
+
         List<ItemInfo> itemInfos = new ArrayList<>();
         itemInfos.add(item1Info);
         itemInfos.add(item2Info);
         itemInfos.add(item3Info);
+        itemInfos.add(item4Info);
+        itemInfos.add(item5Info);
+        itemInfos.add(item6Info);
 
         return itemInfos;
     }
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,79 @@
 package pos.machine;
 
 import java.util.List;
+import static pos.machine.ItemDataLoader.loadAllItemInfos;
 
 public class PosMachine {
+    public final char space = '\n';
+    public int finalTotal = 0;
+    public String itemDetail = "";
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+        List<ItemInfo> itemInfos = loadAllItemInfos();
+        checkBarcodes(barcodes, itemInfos);
+        return generateFinalReceipt(itemDetail, finalTotal);
+    }
+
+    private void checkBarcodes(List<String> barcodes, List<ItemInfo> itemInfos) {
+        int barcodeCounter = 0;
+        int receiptCounter = 0;
+        ItemInfo item = null;
+        int total = 0;
+
+        for(String barcode : barcodes) {
+
+            if(isfDifferentBarcode(item, barcode) || isSingleEqualBarcode(item, barcode, barcodes, receiptCounter)){
+                int quantity = barcodeCounter;
+                total = getTotal(quantity, item.getPrice());
+                barcodeCounter = 0;
+
+                if(isLastBarcode(receiptCounter, barcodes.size())){
+                    quantity++;
+                    total = getTotal(quantity, item.getPrice());
+                }
+
+                itemDetail += getItemDetails(item, quantity, total);
+            }
+
+            item = getItemInfo(itemInfos, barcode);
+
+            barcodeCounter++;
+            receiptCounter++;
+
+            finalTotal += total;
+            total = 0;
+        }
+    }
+
+    private String generateFinalReceipt(String receipt, int finaltotal) {
+        return "***<store earning no money>Receipt***" + space + receipt + "----------------------" + space +
+                "Total: " + finaltotal + " (yuan)"+ space + "**********************";
+    }
+
+    private ItemInfo getItemInfo(List<ItemInfo> itemInfos, String barcode) {
+        return itemInfos.stream()
+                .filter(itemInfo -> itemInfo.getBarcode().equals(barcode))
+                .findFirst()
+                .get();
+    }
+
+    private String getItemDetails(ItemInfo item, int quantity, int total) {
+        return "Name: " + item.getName() + "," + " Quantity: " + quantity + ", Unit price: " + item.getPrice() + " (yuan), Subtotal: " + total + " (yuan)" + space;
+    }
+
+    private boolean isLastBarcode(int receiptCounter, int size) {
+        return receiptCounter+1 == size;
+    }
+
+    private int getTotal(int quantity, int price) {
+        return quantity*price;
+    }
+
+    private boolean isSingleEqualBarcode(ItemInfo item, String barcode, List<String> barcodes, int receiptCounter) {
+        return item != null && barcode.equals(barcodes.get(barcodes.size()-1)) && receiptCounter+1 == barcodes.size();
+    }
+
+    private boolean isfDifferentBarcode(ItemInfo item, String barcode) {
+        return item != null && !barcode.equals(item.getBarcode());
     }
 }
